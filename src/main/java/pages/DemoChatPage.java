e32edac37ff4175de1b41e54954606d4316db193
Dmitry112233 <dmitr.starovoitov@gmail.com>
2019-03-26 09:55:24 +0300

    Project branch init

--- src/main/java/pages/DemoChatPage.java
+++ src/main/java/pages/DemoChatPage.java
@@ -6,7 +6,6 @@ import lombok.extern.log4j.Log4j;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
